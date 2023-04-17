65bb78f173bc4985e6a26d90482e7f143e58fe2f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-17 14:56:55 -0300

    Tipos int e float

---
+++ iniciando_com_python/int_e_float.py
@@ -0,0 +1,15 @@
+print(11) # int
+print(-11) # int
+print(0) # int
+
+
+# floats
+print(1.1)
+print(1.11)
+print(2.50)
+print(0.0)
+
+# Função type()
+print(type("Rafa"))
+print(type(1.1))
+print(type(1))
