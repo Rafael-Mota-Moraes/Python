7bd0b58e0d13a13ebda855edaf7edb7b6d02f044
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-18 11:20:46 -0300

    Variáveis em Python

---
+++ iniciando_com_python/variaveis.py
@@ -0,0 +1,14 @@
+nome_completo = 'Rafael Moreira'
+soma_dois_mais_dois = 2 + 2
+numero_1 = int('1')
+
+print(nome_completo)
+print(soma_dois_mais_dois)
+print(type(numero_1))
+
+nome = 'Rafael'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome: ', nome, ' Idade: ', idade)
+print('É maior? ', maior_de_idade)
