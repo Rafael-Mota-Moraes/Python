f66dfee490e8ff8ea7127f17cbcc6a0c6e860766
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-18 15:17:13 -0300

    f-strings

---
+++ iniciando_com_python/f_strings.py
@@ -0,0 +1,11 @@
+nome = 'Pedro'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+# f-strings
+linha_1 = f'{nome} tem {altura:.2f} de altra'
+linha_2 = f'Pesa {peso}, e seu IMC é {imc:.2f}'
+
+print(linha_1)
+print(linha_2)
