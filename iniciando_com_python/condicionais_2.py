d08941fb8f02156fd9381623b111ddd045c36743
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-19 12:18:56 -0300

    Condicionais 02

---
+++ iniciando_com_python/condicionais_2.py
@@ -0,0 +1,23 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao_1 = False
+condicao_2 = False
+condicao_3 = True
+condicao_4 = False
+
+if condicao_1:
+    print('Código para condição 1')
+elif condicao_2:
+    print('Código para condição 2')
+elif condicao_3:
+    print('Código para condição 3')
+elif condicao_4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição satisfeita')
+
+
+
+
+print('Fora do if')
