8ceae7a93df9dcc8eb8ec5c0cc3d2368357351a5
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-20 17:21:12 -0300

    Operador and

---
+++ iniciando_com_python/and.py
@@ -0,0 +1,14 @@
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+# senha_permitida = '123'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Tabela verdade do true
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
