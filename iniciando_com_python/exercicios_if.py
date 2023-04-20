05c89086e7e166169bfa6fec9902d4e6480c964b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-20 17:04:37 -0300

    Exercicios if e comparação

---
+++ iniciando_com_python/exercicios_if.py
@@ -0,0 +1,12 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if(primeiro_valor > segundo_valor):
+    print('Primeiro valor é maior que o segundo')
+elif(segundo_valor > primeiro_valor):
+    print('Segundo valor é maior que o primeiro')
+elif(segundo_valor == primeiro_valor):
+    print('Valores iguais')
+else:
+    print('Valores iguais')
+
