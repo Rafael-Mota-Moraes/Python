9fea38155c9e2d820607c55a88d338e536b2992e
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-01 10:55:57 -0300

    Introdução às funções Python

---
+++ python_intermediario/def.py
@@ -0,0 +1,22 @@
+
+# print('Função print') # é uma função
+
+# def Print(a, b, c):
+#     print("MSG")
+
+
+# Print('Função Print')
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+
+def saudacao(nome='Sem nome'):
+    print(f'Olá {nome}')
+
+
+saudacao('Rafael')
+saudacao()
