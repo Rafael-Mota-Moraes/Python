9fb0ba2efdfc0e81716f88bb428ba305fb57cdae
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-06 14:22:22 -0300

    Empacotamento e desempacotamento

---
+++ python_intermediario/empacotamento_desempacotamento.py
@@ -0,0 +1,34 @@
+a, b = 1, 2
+a, b = b, a
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza'
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6
+}
+
+pessoa_completa = {
+    **pessoa, **dados_pessoa
+}
+
+print(pessoa_completa)
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+
+mostro_argumentos_nomeados(nome='Joana', qlq=123)
+mostro_argumentos_nomeados(**pessoa_completa)
