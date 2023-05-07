2cfa645c41d4c51cf71e0bb2124e89b573a104aa
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-07 11:40:13 -0300

    Mais detalhes sobre Iterables e Iterators

---
+++ python_intermediario/mais_sobre_iter.py
@@ -0,0 +1,10 @@
+iterable = ['Eu', 'tenho', '__iter__']
+iterator = iter(iterable)
+
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+# print(iterator.__next__())
+# print(iterator.__next__())
+# print(iterator.__next__())
